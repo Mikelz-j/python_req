97370a8aa67418c6597ca72af3ff04983e686d11
Mike_jj <mkokushin18@gmail.com>
2020-08-10 09:56:28 +0300

    drop needless

--- Get.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-print(requests.get('https://pddimp.yandex.ru/api2/admin/email/list?domain=seacomm.ru&page=1&on_page=5 HTTP/1.1').content)
--- hu.py
+++
@@ -1,4 +0,0 @@
-import sys
-for i in range(len(sys.argv)):
-    if i != 0:
-        print(i, end=' ')
